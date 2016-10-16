86a0c3aa6fd8ad752586984e61f95a39e7189a21
Nicolás C <crevs.daak@gmail.com>
2016-10-16 13:01:08 -0300

    i think this should work

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function daak_web_get_random(min, max)
 }
 function daak_web_get_sw_quote()
 {
-  var sw_quotes = ["Welcome to the Daak site!",
+  var sw_quotes = ["null",
+                   "Welcome to the Daak site!",
                    "Hmmm. The Daak site clouds everything. Impossible to see, the future is.",
                    "You don't know the <u>power</u> of the Daak site!",
                    "Your feeble skills are no match for the power of the Daak site!",
@@ -12,6 +13,11 @@ function daak_web_get_sw_quote()
                    "Don't click in to <a href='index.html'>hate</a>. That leads to the Daak site.",
                    "Become my apprentice. Learn to use the Daak site of the Force",
 ];
-  // I don't give a damn if the same result gets repeated, so make it simple
-  document.getElementById("sw_quote").innerHTML = sw_quotes[daak_web_get_random(0, 7)];
+  rand1 = daak_web_get_random(1, 7);
+  if (rand1 = rand2)
+    rand2 = daak_web_get_random(1, 7);
+  else
+    rand2 = rand1
+
+  document.getElementById("sw_quote").innerHTML = sw_quotes[rand2;
 }
